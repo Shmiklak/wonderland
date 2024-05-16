d8c072ea7f8c2c37b0f013bdbb37a47b7569e76d
Doston Shametov <doston.shametov@kpi.com>
2024-05-16 11:11:17 +0500

    add laravel-to-uml dependency

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function results() {
+        return $this->hasMany(GameResult::class, 'user_id', 'id');
+    }
 }
